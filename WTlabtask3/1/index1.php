e45c74ce269e04ff335e64756da47c0a95db1909
kakon-khairulislam <99829285+kakon-khairulislam@users.noreply.github.com>
2022-06-25 03:54:57 +0600

    Completed index1 of labtask3

--- WTlabtask3/1/index1.php
+++ WTlabtask3/1/index1.php
@@ -8,13 +8,20 @@
 </head>
 <body>
     <b>LOGIN  </b><br><br>
-<div class = "one">
-    User Name : <input type="text" name = "UN"><br><br>
-    Password  : <input type="text" name = "PW"> <hr><br>
+ <div class = "one">
+    <form action="login.php" method = "POST">
+    <?php if (isset($_GET['error'])) { ?>
+    <p class="error"><?php echo $_GET['error']; ?></p>
+    <?php } ?>
+    User Name : <input type="text" name = "UN" placeholder = "Enter Username"><br><br>
+    Password  : <input type="text" name = "PW" placeholder = "Password"> <hr><br>
+
 </div>
 <div>
 <input type="checkbox" name = "rem">Remember Me <br><br>
 </div>
 <input type="Submit" name = "submit" id = "sub"> <a href="">Forget Password?</a>
 </body>
+    </form>
+
 </html>
