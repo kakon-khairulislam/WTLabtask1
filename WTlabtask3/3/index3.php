60905d4fe23f82ffa3095f89891051f81a6bae8d
kakon-khairulislam <99829285+kakon-khairulislam@users.noreply.github.com>
2022-06-26 03:20:39 +0600

    Update index3.php

--- WTlabtask3/3/index3.php
+++ WTlabtask3/3/index3.php
@@ -4,9 +4,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel = "stylesheet" href = "c3.css"/>
-    <title>Document</title>
+    <title>Profile picture</title>
 </head>
 <body>
-    
+    <form action="upload.php" method= "POST" class="form" enctype="multipart/form-data">
+        <h1>Profile Picture</h1>
+        <img src="user.jpg" alt="user"><br>
+        <input type="file" name = "pic" id="b1"><hr>
+        <input type="submit" name = "submit">
+    </form>
 </body>
 </html>
