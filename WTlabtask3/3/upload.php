d651dfbb13c0cf503c563e15ac503d7db48ca8fc
kakon-khairulislam <99829285+kakon-khairulislam@users.noreply.github.com>
2022-06-29 22:47:14 +0600

    completed 3 of labtask 3

--- WTlabtask3/3/upload.php
+++ WTlabtask3/3/upload.php
@@ -1,16 +1,32 @@
 <?php 
 if(isset($_POST['submit'])){
-    $file = $_FILES['pic']['name'];
-    if(empty($file)){
-        echo "No File chosen";
+    $file_n = $_FILES['file']['name'];
+    $size = strlen($file_n);
+    $expos = strrpos($file_n,".");
+    $ext = substr($file_n,$expos+1,$size);
+    $e= array('jpeg','jpg','png',);
+    if(empty($file_n)){
+        header("Location: index.php?error= NO file chosen");
+        exit();
     }else{
-        echo "okay";
+      if(in_array($ext,$e) ){
+        if($_FILES['file']['size']>0 && $_FILES['file']['size']<4091300 ){
+              $ta = $_FILES['file']['tmp_name'];
+              $fa = 'Uploaded files/'.uniqid().$file_n;
+              move_uploaded_file($ta,$fa);
+              echo '<img src="'.$fa.'"width="300" height="200">';
+        }else{
+          header("Location: index.php?error= File size can't be more than 4MB ");
+          exit();
+        }
+      }else{
+        header("Location: index.php?error= File type can only be jpeg/jpg/png");
+        exit();
+      }
+      
     }
-
-
-}
-
-
+    
+    }
 
 
 ?>
